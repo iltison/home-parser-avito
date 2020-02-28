8887e074f319f270faf16e155cac06b8bec96d06
iltison <ilyushka123456@gmail.com>
2020-02-28 12:24:10 +0500

    test

--- avito.py
+++ avito.py
@@ -3,45 +3,55 @@ import requests
 import csv
 import pandas as pd
 import numpy as np
-link = ''
+link = 'https://www.avito.ru/asha/doma_dachi_kottedzhi/prodam-ASgBAgICAUSUA9AQ?cd=1&p=4'
 
 
 class avito_parser():
-    def get_total_link(self):
-        pass
-    def get_link(self):
-        pass
-    
-    def parse_link(self):
+    def get_html(self, link):
         response = requests.get(link)
+        return response
+
+    def parse_link(self,response):
         tree = BeautifulSoup(response.content, 'html.parser')
         return tree
-        
-    def processing_data(self,tree):
-        
-        tittle = tree.find_all('a', {'class' : 'snippet-link'})
-        tittle = [tl.text for tl in tittle]
 
-        prices = tree.find_all('span', {'class' : 'snippet-price'}) 
-        prices = [pr.text for pr in prices]
+    def get_pages(self,soup):
+        pages = soup.find('div', class_='pagination-root-2oCjZ').find_all('span', class_='pagination-item-1WyVp')[-2].text
+        return int(pages)
+    
+    def processing_data(self,tree,pages):
+        all_tittle = []
+        all_prices = []
+        all_adress = []
 
-        adress = tree.find_all('span', {'class' : 'item-address__string'}) 
-        adress = [adr.text for adr in adress]
-        array = [tittle,prices,adress]
-        return array
-
-    def save_data(self,array):
-        dic_data = { 'name': array[0],
-        'price': array[1],
-        'adress': array[2]
+        for p in range(pages):
+            tittle = tree.find_all('a', {'class' : 'snippet-link'})
+            for tl in tittle:
+                all_tittle.append(tl.text)
+            prices = tree.find_all('span', {'class' : 'snippet-price'}) 
+            for pr in prices:
+                all_prices.append(pr.text)
+            adress = tree.find_all('span', {'class' : 'item-address__string'}) 
+            for adr in adress:
+                all_adress.append(adr.text)
+        data = { 
+        'name': all_tittle,
+        'price': all_prices,
+        'adress': all_adress
         }
-        df = pd.DataFrame(data = dic_data)
+        return data
+
+    def save_data(self,data):
+        df = pd.DataFrame(data = data)
         df.to_csv('s.csv')
 
     def run(self):
-        tree = self.parse_link()
-        array = self.processing_data(tree)
-        self.save_data(array)
+        response = self.get_html(link)
+        tree = self.parse_link(response)
+        pages = self.get_pages(tree)
+
+        data = self.processing_data(tree,pages)
+        self.save_data(data)
 
 if __name__ == '__main__':
     avito_parser().run()
